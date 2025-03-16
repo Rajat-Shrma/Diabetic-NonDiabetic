f24916c69619e0980f8cbf7e5e1175c8234c3c53
Hidden-Layers-Combinator <hiddenlayers029@gmail.com>
2025-03-16 11:25:49 +0530

    DiabeticsPrediction

---
+++ DiabeticPrediction.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+import sklearn
+import streamlit as st
+import numpy as np
+import time as t
+load_model=pickle.load(open('C:/Users/hp/Downloads/diabeticPrediction.sav','rb'))
+def check(input_data):
+    if load_model.predict(input_data)==1:
+        return "Negative"
+    else:
+        return "Positive"
+    
+
+def main():
+    #Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome
+    st.title("Diabetic Prediction Web App")
+    Pregnancies= st.slider("No. of Pregnancies",0,20)
+    Glucose= st.text_input("Glucose level")
+    BloodPressure= st.text_input("Blood pressure")
+    SkinThickness= st.text_input("Skin Thickness")
+    Insulin= st.text_input("Insulin")
+    BMI= st.text_input("BMI")
+    DiabetesPF= st.text_input("DiabetesPedigreeFunction")
+    Age= st.text_input("Age")
+    data=[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPF,Age]
+    input_narray=np.asarray(data)
+    input_data=input_narray.reshape(1,-1)
+    if st.button("Report"):
+        
+        st.spinner("just wait")
+        with st.spinner("just wait"):
+            t.sleep(2)
+
+        report=check(input_data)
+        if report=="Positive":
+            st.success("You Non diabetic Person")
+        else:
+            st.warning("You are a diabetic Person.Get yourself checked!")
+
+if __name__=="__main__":
+    main()
